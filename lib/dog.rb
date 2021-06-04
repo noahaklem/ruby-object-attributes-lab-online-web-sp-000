b0197aeb4048493a08ade87c3c4128812dfb797b
Noah  Klem <klem.noah@gmail.com>
2021-06-04 17:56:46 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+class Dog
+  def name
+    @that_dogs_name
+  end
+  def name=(dog_name)
+    @that_dogs_name = dog_name
+  end
+  def breed
+    @that_dogs_breed
+  end
+  def breed=(dog_breed)
+    @that_dogs_breed = dog_breed
+  end
+end
